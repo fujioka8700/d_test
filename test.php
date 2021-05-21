0f1b30edd645b1c881791f4fcc5b6a0f6473409c
fujioka8700 <fujioka8700@gmail.com>
2021-05-21 15:08:09 +0900

    Review of docker

--- test.php
+++ test.php
@@ -1,5 +1,12 @@
-<?php
-  echo "Hello World";
-  echo "Hello World";
-  echo "Hello World";
-?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>うひょお</title>
+</head>
+<body>
+  <p>わっしょい♪わっしょい♪</p>
+</body>
+</html>
